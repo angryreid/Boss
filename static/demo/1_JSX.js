151499c749e1a0e45ddfdb5055176dcac4d994a5
angryreid <dxiang1391458323@foxmail.com>
2019-04-22 11:56:28 +0800

    update

--- static/demo/1_JSX.js
+++ static/demo/1_JSX.js
@@ -29,9 +29,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>{formatName(derek)}</p>
+          <p>{formatName(user)}</p>
           <p>{formatName()}</p>
-          {getGreeting(derek)}
+          {getGreeting(user)}
           {getGreeting()}
           <a  
             className="App-link"
