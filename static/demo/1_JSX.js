f0222471a03a30c5f8473e674fb8c0589f613420
angryreid <dxiang1391458323@foxmail.com>
2019-04-22 11:56:03 +0800

    update

--- static/demo/1_JSX.js
+++ static/demo/1_JSX.js
@@ -2,8 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function Welcome(props){
-  return <p>Hello, {props.name}</p>
+function formatName(user){
+  if(user){
+    return user.firstName +' '+ user.lastName;
+  }else{
+    return 'stranger'
+  }
+}
+
+function getGreeting(user){
+  if(user){
+    return <p>Hello,{formatName(user)}</p>
+  }else{
+    return <p>Stranger!</p>
+  }
+}
+
+const user = {
+  firstName: 'foo',
+  lastName: 'bar'
 }
 
 class App extends Component {
@@ -12,17 +29,17 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <div className="coding-area">
-            <Welcome name="derek"></Welcome>
-            <Welcome name="emma"></Welcome>
-          </div>
+          <p>{formatName(derek)}</p>
+          <p>{formatName()}</p>
+          {getGreeting(derek)}
+          {getGreeting()}
           <a  
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Hello Derek
+            Hello World
           </a>
         </header>
       </div>
