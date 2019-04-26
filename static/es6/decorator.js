9096b4d925fd4db696f6e779ff4a41ed1da19cb1
angryreid <dxiang1391458323@foxmail.com>
2019-04-26 11:25:35 +0800

    update

--- static/es6/decorator.js
+++ static/es6/decorator.js
@@ -2,8 +2,40 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+/**
+ * decorator 装饰器
+ * 当前案例无法运行，提示无法使用装饰器，
+ * 具体描述 https://github.com/facebook/create-react-app/issues/5708
+ */
+
+const args = {
+  f1() {
+    console.log("f1");
+  },
+  f2() {
+    console.log("f2");
+  },
+  f3() {
+    console.log("f3");
+  }
+};
+
+// 定义装饰器的外层函数
+function mixin(...args) {
+  // 返回一个装饰器函数
+  return function(target) {
+    Object.assign(target.proptype, ...args);
+  };
+}
+
+// 使用mixins装饰器
+@mixin(args)
+class Animal {}
+
 class App extends Component {
   render() {
+    let cat = new Animal();
+    cat.f1();
     return (
       <div className="App">
         <header className="App-header">
