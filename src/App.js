99f65ebb63778c3f8e7816f7bdd6df77655ad1cf
dengxiang <angried@163.com>
2019-07-10 15:42:17 +0800

    something

--- src/App.js
+++ src/App.js
@@ -2,25 +2,19 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import Hello from "./components/Hello"
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <div className="coding-area">
             {/* start */}
-   
+            <Hello></Hello>
             {/* end */}
           </div>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Hello Derek
-          </a>
         </header>
       </div>
     );
