3c5ebc12e2562f75f282dda9f0c41f6bbb2135ac
dengxiang <angried@163.com>
2019-07-25 15:48:12 +0800

    add babel-plugin-import

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React, { Component } from "react";
-import "./App.css";
+import { Button } from 'antd-mobile'
 
+import 'antd-mobile/dist/antd-mobile.css'
 
 class App extends Component {
   render() {
+    let boss = "derek";
     return (
-      <div className="App">
-        <header className="App-header">
-          <div className="coding-area">
-           <p>jjj</p>
-          </div>
-        </header>
+      <div>
+        <div>hello,{boss}</div>
+        <One />
+      </div>
+    );
+  }
+}
+
+class One extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: ["he", "she", "him", "her"]
+    };
+  }
+
+
+
+
+  addPeople(){
+    this.setState({
+      people: [...this.state.people,'ooooo' + Math.random(10)]
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Button type="ghost" onClick={() => this.addPeople()}>add</Button>
+        <ul>
+          {this.state.people.map(v => {
+            return <li key={v}>{v}</li>;
+          })}
+        </ul>
       </div>
     );
   }
