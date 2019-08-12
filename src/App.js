9b3ce908a8ba5cb179dd12c9f038698c84f306d6
dengxiang <angried@163.com>
2019-08-12 14:11:53 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Xiaojiejie from "./components/Xiaojiejie";
 
 import "./App.css";
 
@@ -9,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <div className="coding-area">
-            <Xiaojiejie />
+           
           </div>
         </header>
       </div>
--- src/components/Test.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-class App extends Component {
-  state = {  }
-  render() { 
-    return ;
-  }
-}
- 
-export default App;
