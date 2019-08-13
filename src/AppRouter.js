5c93635b1b54b0d473e8c3cb10c9df10ad883646
dengxiang <angried@163.com>
2019-08-13 13:39:17 +0800

    update

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,28 +1,15 @@
 import React from "react";
-import Index from "./pages/Index";
-import List from "./pages/List";
-import Home from "./pages/Home";
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-const AppRouter = () => {
+function AppRouter(){
   return (
     <Router>
-      <ul>
-        <li>
-          <Link to="/">home</Link>
-        </li>
-        <li>
-          <Link to="/list/">list</Link>
-        </li>
-      </ul>
-      {/* exact 表示精确匹配 */}
-      <Route path="/" exact component={Index} />
-      <Route path="/list/:id" exact component={List} />
-      <Route path="/home/" exact component={Home} />
-
+      <div className="mainDiv">
+        <div className="leftNav"></div>
+      </div>
     </Router>
-  );
-};
+  )
+}
 
 export default AppRouter;
