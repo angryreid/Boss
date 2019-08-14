26d586ade0e1784fe2daf5d546b24fd1989c215a
dengxiang <angried@163.com>
2019-08-14 16:32:27 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./assets/css/base.css"
 
-import Hello from "./components/Hello"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <div className="coding-area">
-            {/* start */}
-            <Hello></Hello>
-            {/* end */}
-          </div>
-        </header>
       </div>
     );
   }
